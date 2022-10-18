85cbecd4dc6cd7fafd8e4d5ec2786775aace9c1e
andymarlow <andymarlow4@gmail.com>
2022-10-18 15:46:48 +0100

    find-match css tidy up

--- components/FindMatchCard.js
+++ components/FindMatchCard.js
@@ -1,6 +1,5 @@
 import Link from "next/link";
 import articleStyles from "../styles/Article.module.css";
-import Image from "next/image";
 
 export default function FindMatchCard({ match }) {
   const time = match.date.slice(0, 16).replace("T", " ");
@@ -9,8 +8,8 @@ export default function FindMatchCard({ match }) {
     <>
       <main>
         <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-          <div className="lg:p-6 md:w-full flex justify-center mt-40">
-            <div className="max-w-xl rounded-2xl overflow-hidden shadow-lg bg-gray-800 text-white">
+          <div className="lg:p-6 md:w-full flex justify-center mt-10">
+            <div className="w-1/2 gap-y-2 rounded-2xl  overflow-hidden shadow-lg bg-gray-800 text-white">
               <img
                 className="w-full"
                 src={`/images/${match.pitch.name}.jpeg`}
@@ -21,14 +20,12 @@ export default function FindMatchCard({ match }) {
                   {match.pitch.name}
                   <br></br>
                   {time}
-                  <br></br>
-                  {match._id.players}
                 </h1>
               </div>
               {/* button styling */}
-              <div>
+              <div className="pb-3 pl-3">
                 <Link href="/find-match/[_id]" as={`/find-match/${match._id}`}>
-                  <button className="right-5 bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-full">
+                  <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-full">
                     Match Details
                   </button>
                 </Link>
